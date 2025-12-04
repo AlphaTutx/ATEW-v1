6bafc3b94bb50ca241adc3d1ac23026f37e17888
Mark John <alpha.tutx@gmail.com>
2025-12-04 19:56:35 +0800

    Fix signup link in index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
   	 </div>
 
   	<p>
-  		Not yet a member? Please contact us. <a href="https://www.facebook.com/profile.php?id=100087598551338">Sign up</a>
+  		Not yet a member? Please contact us. <a href="register.phpregister.php">Sign up</a>
   	</p> 
 
   </form>
