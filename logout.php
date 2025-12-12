ca00c005856a08a0293ef107756dcab57443451a
Mark John <alpha.tutx@gmail.com>
2025-12-13 06:48:54 +0800

    Fix db links

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (isset($_SESSION['username'])) {
-    $db = mysqli_connect('localhost', 'root', '', 'project');
+    $db = mysqli_connect('sql100.infinityfree.com', 'if0_40507997', 'JWtgJvb8Zyyql', 'if0_40507997_project');
     $username = $_SESSION['username'];
 
     // Mark as logged out
